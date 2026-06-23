82937d87a301a522afc6c6a93a0853fd18d95923
Parashar-Nitin <nparashar306@gmail.com>
2026-06-22 21:54:37 -0300

    .

--- Day006BinarySearch/Solution.java
+++ Day006BinarySearch/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int start=0, end=nums.length-1, result=-1;
         while(start<=end)
         {
-            int mid=(start+end)/2;
+            int mid=start + (end-start)/2;
             if(nums[mid]==target) return mid;
             else if(nums[mid]<target) start=mid+1;
             else end=mid-1;
